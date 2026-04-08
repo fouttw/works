63c182f6e60f8eb250e6777c7011fdb578fdc223
fouttw <al336love@gmail.com>
2026-04-08 16:11:17 +0300

    Add files via upload

---
+++ калькулятор.py
@@ -0,0 +1,12 @@
+a = int(input (""))
+b = int(input (""))
+print ("выберете действие")
+print ("1- сложение")
+print ("2 - вычитание")
+c = input ()
+if c == "1":
+    print (a+b)
+elif c == "2":
+    print (a-b) 
+else:
+    print ("нет")
